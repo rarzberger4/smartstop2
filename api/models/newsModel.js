2e2169959e248cf50c63982ca8a818cad27ad3a8
FischerCarmen <91220946+FischerCarmen@users.noreply.github.com>
2022-06-17 11:36:47 +0200

    Add files via upload

--- api/models/newsModel.js
+++ api/models/newsModel.js
@@ -1,7 +1,14 @@
-class News{
-    //Constructor
-}
+const NewsAPI = require('newsapi');
+const key = '91575b8a494a4a2a942befe9c9095a55';
+
+const news = new NewsAPI(key);
+
 
 class NewsModel{
-    //Methods to add News to the webpage
+    getNews(){
+        return news.v2.sources({
+            q: 'Vienna',
+            language: 'en'
+        }).then(responseNews => JSON.parse(responseNews))
+    }
 }
--- api/models/weatherModel.js
+++ api/models/weatherModel.js
@@ -1,7 +1,18 @@
 class Weather{
-    //Constructor
+    constructor(temp, conditionIcon, conditionText, uv) {
+        this.temp = temp;
+        this.conditionIcon = conditionIcon;
+        this.conditionText = conditionText;
+        this.uv = uv;
+    }
 }
 
 class WeatherModel{
     //Methods to add Weather to the webpage
+    getWeather(){
+        return fetch('https://api.weatherapi.com/v1/current.json?key=5919a75387b14f14be3154456221104&q=Vienna&aqi=no')
+            .then(responseWeather => responseWeather.json())
+            .then(weather => JSON.parse(weather))
+            .then(text => new Weather(text.current.temp_c, text.current.condition.icon, text.current.condition.text, text.current.uv))
+    }
 }
